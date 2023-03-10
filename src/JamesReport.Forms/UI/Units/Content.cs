9b6643f7d26478ed3d1363e1b481dd6cfd721b03
JAEWUNG LEE <james@jamesnet.dev>
2023-03-11 05:33:27 +0900

    Updates

---
+++ src/JamesReport.Forms/UI/Units/Content.cs
@@ -0,0 +1,13 @@
+﻿using JamesReport.Core;
+using System.Windows;
+
+namespace JamesReport.Forms.UI.Units
+{
+    public class Text : DragMoveContent
+    {
+        static Text()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Text), new FrameworkPropertyMetadata(typeof(Text)));
+        }
+    }
+}
--- src/JamesReport.Forms/UI/Units/Header.cs
+++ src/JamesReport.Forms/UI/Units/Header.cs
@@ -3,7 +3,7 @@ using System.Windows;
 
 namespace JamesReport.Forms.UI.Units
 {
-    public class Header : DragMoveContent
+    public class Header : Text
     {
         static Header()
         {
